9348e4683b75a3b95fa9e32c6d98d5c442213075
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-12-02 00:07:22 -0500

    added return types to Riegel and Cameron models

--- pyfit/model/aerobic.pxd
+++ pyfit/model/aerobic.pxd
@@ -2,14 +2,14 @@ cdef class Riegel:
     cdef readonly double d1
     cdef readonly double t1
 
-    cpdef distance(self, double t2)
+    cpdef double distance(self, double t2)
 
-    cpdef time(self, double d2)
+    cpdef double time(self, double d2)
 
 cdef class Cameron:
     cdef readonly double d1
     cdef readonly double t1
 
-    cdef __f__(self, double x)
+    cdef double __f__(self, double x)
 
     cpdef time(self, double d2)
--- pyfit/model/aerobic.pyx
+++ pyfit/model/aerobic.pyx
@@ -1,32 +1,32 @@
 from libc.math cimport pow
 
-cdef class Riegel(object):
+cdef class Riegel:
 
     def __cinit__(self, double d1, double t1):
         self.d1 = d1
         self.t1 = t1
 
-    cpdef distance(self, double t2):
+    cpdef double distance(self, double t2):
         if t2 <= 0:
             return 0
         return self.d1*(pow(t2, 50.0/53.0)/pow(self.t1, 50.0/53.0))
 
-    cpdef time(self, double d2):
+    cpdef double time(self, double d2):
         if d2 <= 0:
             return 0
         return self.t1 * pow( (d2/self.d1), 1.06)
 
-cdef class Cameron(object):
+cdef class Cameron:
 
     def __cinit__(self, double d1, double t1):
         self.d1 = d1
         self.t1 = t1
 
 
-    cdef __f__(self, double x):
+    cdef double __f__(self, double x):
         return 13.49681 - 0.048865*x + 2.438936/pow(x,0.7905)
 
-    cpdef time(self, double d2):
+    cpdef double time(self, double d2):
         cdef double d1Miles = self.d1 / 1609.34
         cdef double d2Miles = d2 / 1609.34
         if d2 <= 0:
